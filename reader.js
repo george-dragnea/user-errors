e58a562c7b2afe680a347598a7b26a5129185987
George <georgedragnea07@gmail.com>
2018-11-16 23:58:58 +0200

    eslint, connect to mongodb, write log, and read...very basic

--- reader.js
+++ reader.js
@@ -1,12 +1,25 @@
-var http = require('http');
+const http = require('http');
+const mongodb = require('mongodb');
+const config = require('./config.json');
 
-var port = 9999;
+// Connect to MongoDB database
+let db;
+mongodb.MongoClient.connect(config.database.mongodb.url,
+  { useNewUrlParser: true },
+  (err, database) => {
+    if (err) throw err;
+    db = database.db(config.database.mongodb.database);
+    console.log('Connected to MongoDB database.');
+  });
 
-var server = http.createServer().listen(port);
-
-server.on('request', function (req, res) {
-    res.write('Hello World!');
+// Create a HTTP server to listen
+const server = http.createServer().listen(config.reader.port, config.reader.ip);
+server.on('request', (req, res) => {
+  db.collection(config.database.mongodb.collection).find({}).toArray((err, result) => {
+    if (err) throw err;
+    res.write(JSON.stringify(result));
     res.end();
+  });
 });
 
-console.log('Listening on port ' + port);
+console.log('Reader listening on', config.reader.ip, config.reader.port);
--- writer.js
+++ writer.js
@@ -1,21 +1,42 @@
-var http = require('http');
-var querystring = require('querystring');
-var qs = require('qs');
+const http = require('http');
+const { parse } = require('querystring');
+const mongodb = require('mongodb');
+const config = require('./config.json');
 
-var port = 8888;
+// Connect to MongoDB database
+let db;
+mongodb.MongoClient.connect(config.database.mongodb.url,
+  { useNewUrlParser: true },
+  (err, database) => {
+    if (err) throw err;
+    db = database.db(config.database.mongodb.database);
+    console.log('Connected to MongoDB database.');
+  });
 
-var server = http.createServer().listen(port);
-
-server.on('request', function (req, res) {
-    if (req.method == 'POST') {
-        var body = '';
-        req.on('data', function (data) {
-            body += data;
+// Create a HTTP server to listen
+const server = http.createServer().listen(config.writer.port, config.writer.ip);
+server.on('request', (req, res) => {
+  if (req.method === 'POST') {
+    let body = '';
+    req.on('data', (chunk) => {
+      body += chunk.toString();
+    });
+    req.on('end', () => {
+      const post = parse(body);
+      const log = {
+        project: 'project' in post ? post.project : null,
+        user: 'user' in post ? post.user : null,
+        error: 'error' in post ? post.error : null,
+      };
+      // Insert log into database
+      if (log.error !== null) {
+        db.collection(config.database.mongodb.collection).insertOne(log, (err) => {
+          if (err) console.log(err);
         });
-        req.on('end', function () {
-            var data = qs.parse(body);            
-        });
-    }
+      }
+      res.end();
+    });
+  }
 });
 
-console.log('Listening on port ' + port);
+console.log('Writer listening on', config.writer.ip, config.writer.port);
