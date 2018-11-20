cebaaca69f7c7c0cbef5c06725f6d4ae7176c16e
George <georgedragnea07@gmail.com>
2018-11-20 22:01:02 +0200

    add buffer to writer

--- writer.js
+++ writer.js
@@ -3,6 +3,7 @@ const { parse } = require('querystring');
 const mongodb = require('mongodb');
 const config = require('./config.json');
 
+let logs = [];
 // Connect to MongoDB database
 let db;
 mongodb.MongoClient.connect(config.database.mongodb.url,
@@ -23,23 +24,35 @@ server.on('request', (req, res) => {
     });
     req.on('end', () => {
       const post = parse(body);
-      const log = {
-        project: 'project' in post ? post.project : null,
-        user: 'user' in post ? post.user : null,
-        error: 'error' in post ? post.error : null,
-        ip: 'ip' in post ? post.ip : null,
-        url: 'url' in post ? post.url : null,
-        extra: 'extra' in post ? post.extra : null,
-      };
-      // Insert log into database
-      if (log.error !== null) {
-        db.collection(config.database.mongodb.collection).insertOne(log, (err) => {
-          if (err) console.log(err);
-        });
+      if ('error' in post) {
+        const log = {
+          project: 'project' in post ? post.project : null,
+          user: 'user' in post ? post.user : null,
+          error: 'error' in post ? post.error : null,
+          ip: 'ip' in post ? post.ip : null,
+          url: 'url' in post ? post.url : null,
+          extra: 'extra' in post ? post.extra : null,
+        };
+        const logIndex = logs.findIndex(item => JSON.stringify(item.log) === JSON.stringify(log));
+        if (logIndex !== -1) {
+          logs[logIndex].count += 1;
+        } else {
+          logs.push({ log, count: 1, date: new Date() });
+        }
       }
       res.end();
     });
   }
 });
+
+// Insert log into database
+setInterval(() => {
+  if (logs.length > 0) {
+    db.collection(config.database.mongodb.collection).insertMany(logs, (err) => {
+      if (err) console.log(err);
+    });
+    logs = [];
+  }
+}, config.writer.bufferTime);
 
 console.log('Writer listening on', config.writer.ip, config.writer.port);
